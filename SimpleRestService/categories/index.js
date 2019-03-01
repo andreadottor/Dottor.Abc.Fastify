a1288431b45affbced7a5df362c3500cadfff942
Andrea Dottor <andrea@dottor.net>
2019-03-01 14:47:17 +0100

    Update index.js

--- SimpleRestService/categories/index.js
+++ SimpleRestService/categories/index.js
@@ -1,6 +1,13 @@
+var categories =[
+    {id: 1, name :'categoria 1' },
+    {id: 2, name :'categoria 2' },
+];
+
 async function routes (fastify, options) {
+    
+    
     fastify.get('/', async (request, reply) => {
-      return ['categoria1', 'categoria2'];
+      return categories;
     })
   }
   
